abaabff5433576cee35d472405509d9515aa3931
HiedanoAjuu <2703998852@qq.com>
2024-09-22 10:48:41 +0800

    Site updated: 2024-09-22 10:48:40

--- dist/music.js
+++ dist/music.js
@@ -7,9 +7,9 @@ const ap = new APlayer({
     lrcType: 0,
     audio: [
         {
-            name: "装飾戦 ～ Decoration Battle",
+            name: "Break the Sabbath",
             artist: "上海アリス幻樂団",
-            url: "http://music.163.com/song/media/outer/url?id=510039.mp3",
+            url: "http://music.163.com/song/media/outer/url?id=510043.mp3",
             cover:
                 "https://p1.music.126.net/zM4G5cgsfQxsgDftD4RJIw==/109951169299510813.jpg",
         }
