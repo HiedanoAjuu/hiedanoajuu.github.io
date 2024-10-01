254b9dafc06ab38d551405b5b5007114607a3f1d
HiedanoAjuu <2703998852@qq.com>
2024-10-01 09:24:36 +0800

    Site updated: 2024-10-01 09:24:35

--- dist/music.js
+++ dist/music.js
@@ -7,9 +7,9 @@ const ap = new APlayer({
     lrcType: 0,
     audio: [
         {
-            name: "Break the Sabbath",
+            name: "紅響曲 ～ Scarlet Phoneme",
             artist: "上海アリス幻樂団",
-            url: "http://music.163.com/song/media/outer/url?id=510043.mp3",
+            url: "http://music.163.com/song/media/outer/url?id=510047.mp3",
             cover:
                 "https://p1.music.126.net/zM4G5cgsfQxsgDftD4RJIw==/109951169299510813.jpg",
         }
